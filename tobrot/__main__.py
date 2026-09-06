8265ef7d47930e922d029e7e111016f88b5e8cdd
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2026-09-06 15:22:00 +0500

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -81,6 +81,7 @@ if __name__ == "__main__":
         api_id=APP_ID,
         api_hash=API_HASH,
         workers=343,
+        sleep_threshold=60,
     )
     #
     incoming_message_handler = MessageHandler(
